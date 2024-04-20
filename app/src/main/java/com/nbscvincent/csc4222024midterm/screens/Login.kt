0a2d0ab62b299773c204a521825fa2304b358bad
jsimangcastudent <147906200+jsimangcastudent@users.noreply.github.com>
2024-04-20 12:55:45 +0800

    Initial
    Initial

--- app/src/main/java/com/nbscvincent/csc4222024midterm/screens/Login.kt
+++ app/src/main/java/com/nbscvincent/csc4222024midterm/screens/Login.kt
@@ -14,6 +14,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,24 +25,46 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.Visibility
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.nbscvincent.csc4222024midterm.model.AppScreenViewModel
+import com.nbscvincent.csc4222024midterm.model.LoginReturn
+import com.nbscvincent.csc4222024midterm.model.ScreenViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Login() {
-    var username by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+fun Login(
+    navController: NavHostController,
+    screenViewModel: ScreenViewModel,
+) {
+    val loginModel = viewModel<AppScreenViewModel>()
+    val coroutineScope = rememberCoroutineScope()
+
+    var username by remember { mutableStateOf("kminchelle") }
+    var password by remember { mutableStateOf("0lelplR") }
     var passwordShow: Boolean by remember { mutableStateOf(false) }
+
+    val openDialog = remember { mutableStateOf(false) }
+    if (openDialog.value){
+        DialogAlert(openDialog.value) { openDialog.value = false }
+    }
 
     Scaffold { innerPadding ->
         Column(
@@ -60,7 +84,7 @@ fun Login() {
                 value = username,
                 shape = RoundedCornerShape(10.dp),
                 onValueChange = { username = it },
-                label = { Text(text = "Enter your email") },
+                label = { Text(text = "Username") },
                 colors = TextFieldDefaults.outlinedTextFieldColors(
                     focusedBorderColor = Color.Black,
                     unfocusedBorderColor = Color.Black,
@@ -103,6 +127,34 @@ fun Login() {
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                 visualTransformation = if (passwordShow) VisualTransformation.None else PasswordVisualTransformation(),
             )
+            Spacer(modifier = Modifier.height(50.dp))
+            Button(
+                onClick = {
+                    coroutineScope.launch {
+                        val loginData = loginModel.checkLogin(username, password)
+                        if (loginData[0].flag == 1){
+                            openDialog.value = true
+                        }else{
+                            screenViewModel.setLogin()
+                            navController.navigate(MainNav.HomePage.name)
+                        }
+                    }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 25.dp, end = 25.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF5742f5)
+                ),
+                shape = RoundedCornerShape(10.dp),
+            ) {
+                Text(
+                    text = "Sign In",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                )
+            }
 
         }
     }
